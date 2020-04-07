688b425235195213d46cfbbd28594b9ba82087dc
Pedro Matheus <pedro.matheus123@gmail.com>
2020-04-07 07:34:56 -0300

    Scopes for sorting reports

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,8 @@
 class Report < ApplicationRecord
   scope :search, lambda { |query| where(['description LIKE ?', "%#{query}%"]) }
+  scope :newest_first, lambda { |query| order('created_at DESC') }
+  scope :oldest_first, lambda { |query| order('created_at ASC') }
+  scope :updated_recently, lambda { |query| order('updated_at DESC') }
+  scope :updated_oldest, lambda { |query| order('updated_at ASC') }
   belongs_to :user
 end
