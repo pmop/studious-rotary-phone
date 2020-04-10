7685470f76537a13b54b6ea7ceaf0976f5404e12
Pedro Matheus <pedro.matheus123@gmail.com>
2020-04-10 03:13:28 -0300

    Json serializing methods

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,4 +5,22 @@ class Report < ApplicationRecord
   scope :updated_recently, lambda { order('updated_at DESC') }
   scope :updated_oldest, lambda { order('updated_at ASC') }
   belongs_to :user
+
+  def as_json
+    JSON.generate({
+        status:      self.status,
+        description: self.description,
+        user_name:   self.user.name ,
+        user_email:  self.user.email,
+        lat:         self.lat,
+        lng:         self.lng,
+        response:    self.response,
+        created_at:  self.created_at,
+        updated_at:  self.updated_at
+    })
+  end
+
+  def to_json
+    as_json
+  end
 end
