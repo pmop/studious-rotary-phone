201b5293699a572a2539d5a982f0e86dfeba3825
Pedro Matheus <pedro.matheus123@gmail.com>
2020-04-10 03:13:53 -0300

    Support refresh_by_destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   post 'refresh', controller: :refresh, action: :create
   post 'auth', controller: :auth, action: :create
   delete 'auth', controller: :auth, action: :destroy
+  # delete 'auth/destroy_by_refresh', to: 'auth#destroy_by_refresh', as: :destroy_by_refresh 
   post 'signup', controller: :signup, action: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
